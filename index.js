a07ae65e55cb822ab558c79bc684a652dd8a7de1
krushndayshmookh <krushndayshmookh@gmail.com>
2018-04-24 22:40:39 +0530

    setup server

---
+++ index.js
@@ -0,0 +1,63 @@
+// ----- Initialize Express -----
+
+var express = require('express');
+var app = express();
+
+
+
+
+// ----- Configuration -----
+
+var port = 3000;
+
+var routes = require("./routes.js");
+
+
+
+
+// ----- Middleware -----
+
+var bodyParser = require('body-parser');
+
+//To parse URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//To parse json data
+app.use(bodyParser.json());
+
+// -----
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// -----
+
+app.use(express.static('www'));
+
+// -----
+
+app.use('/', routes);
+
+// -----
+
+
+
+
+
+
+
+
+
+// ----- Start listening -----
+
+app.listen(port, function (err) {
+
+    if (err) {
+        throw err;
+    }
+
+    console.log("App listening on port " + port);
+
+});
