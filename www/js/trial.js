9a3788a2c5ec55e9557d7812dd8b4a0497b91727
krushndayshmookh <krushndayshmookh@gmail.com>
2018-04-29 15:40:38 +0530

    created homepage mockup

--- www/js/trial.js
+++ www/js/trial.js
@@ -1,3 +1,17 @@
+$(document).ready(function () {
 
-var elem = document.querySelector('.parallax');
-var instance = M.Parallax.init(elem);
+
+    var tiltIntroHead = $('#intro-head').tilt({
+        glare: true,
+        maxTilt: 5
+        //disableAxis: "y"
+    });
+
+    var tiltBanner1 = $('#banner-1').tilt({
+        maxTilt: 3,
+        disableAxis: "y"
+    });
+});
+
+//var scene = document.getElementById('scene');
+//var parallaxInstance = new Parallax(scene);
