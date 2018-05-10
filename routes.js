a651007d45261d3ec40d595f4a321d925ff51c1d
Sahil Kindarle <shieldanima@Sahils-MacBook-Air.local>
2018-05-10 10:56:14 +0530

    chnge

--- routes.js
+++ routes.js
@@ -13,20 +13,7 @@ router.get('/trial', (req, res) => res.render('trial'));
 
 router.get('/homepage', (req, res) => res.render('homepage'));
 
-<<<<<<< HEAD
-router.get('/gallery', function (req, res) {
-    res.render('gallery');
-});
-
-router.get('/ec', function (req, res) {
-    res.render('e-comm');
-});
-=======
-router.get('/ec', (req, res) => res.render('e-comm'));
->>>>>>> origin/master
-
-
-
+router.get('/gallery', (req, res) => res.render('gallery'));
 
 //export this router to use in our index.js
 module.exports = router;
