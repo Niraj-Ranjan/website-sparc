d73a59cb4a340108b79b766cfbbfcb0143d0057b
krushndayshmookh <krushndayshmookh@gmail.com>
2018-05-10 14:35:12 +0530

    add schemas and models

---
+++ models/project.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ProjectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    owner: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    description: {
+        type: String,
+        max: 1000
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    url: {
+        type: String,
+    },
+    categories: [{
+        type: String,
+        max: 100
+    }]
+});
+
+// Virtual for product's images
+
+ProjectSchema
+    .virtual('images')
+    .get(function () {
+        return '/catalog/project/' + this._id;
+    });
+
+//Export model
+module.exports = mongoose.model('Project', ProjectSchema);
+
