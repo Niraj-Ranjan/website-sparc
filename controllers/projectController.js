dc996f1fc20e23a9c6fdcbb9041e916bac943fe1
krushndayshmookh <krushndayshmookh@gmail.com>
2018-06-05 20:42:56 +0530

    route for edit-project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,41 +1,64 @@
 var Project = require('../models/project');
 
 // Display list of all Projects.
-exports.project_list = function(req, res) {
+exports.project_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Project list');
 };
 
+exports.project_edit = function (req, res) {
+    Project.find({})
+        .exec(function (err, list_projects) {
+            if (err) {
+                throw err;
+            }
+            //Successful, so render
+            res.render('edit-projects', {
+                projects: list_projects
+            });
+            //res.send(list_projects);
+        });
+};
+
 // Display detail page for a specific Project.
-exports.project_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Project detail: ' + req.params.id);
+exports.project_detail = function (req, res) {
+    Project.findById(req.params.id)
+        .exec(function (err, project) {
+            if (err) {
+                throw err;
+            }
+            //Successful, so render
+            //console.log(product)
+            res.send(project);
+            //res.send(list_products);
+        });
 };
 
 // Display Project create form on GET.
-exports.project_create_get = function(req, res) {
+exports.project_create_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Project create GET');
 };
 
 // Handle Project create on POST.
-exports.project_create_post = function(req, res) {
+exports.project_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Project create POST');
 };
 
 // Display Project delete form on GET.
-exports.project_delete_get = function(req, res) {
+exports.project_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Project delete GET');
 };
 
 // Handle Project delete on POST.
-exports.project_delete_post = function(req, res) {
+exports.project_delete_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Project delete POST');
 };
 
 // Display Project update form on GET.
-exports.project_update_get = function(req, res) {
+exports.project_update_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Project update GET');
 };
 
 // Handle Project update on POST.
-exports.project_update_post = function(req, res) {
+exports.project_update_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Project update POST');
 };
