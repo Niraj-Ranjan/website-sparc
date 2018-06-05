508049b019e42fd7f10b071d23153fa6c7112fcf
krushndayshmookh <krushndayshmookh@gmail.com>
2018-06-05 17:05:49 +0530

    bugfix, login, other routes

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 
@@ -16,13 +16,21 @@ router.get('/about', function (req, res) {
     res.render('about');
 });
 
+router.get('/privacypolicy', function (req, res) {
+    res.render('privacypolicy');
+});
+
+
+/* implemented in project routes -----
 router.get('/gallery', function (req, res) {
     res.render('gallery');
 });
-
+*
+/* Implemented in product routes -----
 router.get('/shop', function (req, res) {
     res.render('shop');
 });
+*/
 
 // -----
 
@@ -32,57 +40,47 @@ router.get('/login', function (req, res) {
     res.render('login');
 });
 
+router.post('/login', function (req, res) {
+    //- console.log(req.body);
+    if (req.body.username == 'sagar' && req.body.password == 'sparc') {
+        res.redirect('/dashboard');
+    } else {
+        res.render('login', {
+            failure: true
+        });
+    }
+
+});
+
 /*router.get('/dashboard', function (req, res) {
     res.render('dashboard');
 });*/
-
-router.get('/updatepro', function (req, res) {
-    res.render('updatepro');
-});
-
-router.get('/addpro', function (req, res) {
-    res.render('addpro');
-});
-
-router.get('/delpro', function (req, res) {
-    res.render('delpro');
-});
 
 
 
 // Data Routes -----
 
-var async = require('async')
+// var async = require('async')
 
 // Require controller modules.
 var enquiry_controller = require('./controllers/enquiryController');
 var product_controller = require('./controllers/productController');
-var productcategory_controller = require('./controllers/productcategoryController');
+// var productcategory_controller = require('./controllers/productcategoryController');
 var project_controller = require('./controllers/projectController');
 
 
 
-// GET catalog home page.
-router.get('/shop', function (req, res) {
-    res.send('NOT IMPLEMENTED: Shop');
-});
+
 
 /// ENQUIRY ROUTES ///
 
-// GET request for creating a Enquiry. NOTE This must come before routes that display Enquiry (uses id).
-router.get('/enquiry/create', enquiry_controller.enquiry_create_get);
+
 
 // POST request for creating Enquiry.
 router.post('/enquiry/create', enquiry_controller.enquiry_create_post);
 
-// GET request to delete Enquiry.
-router.get('/enquiry/:id/delete', enquiry_controller.enquiry_delete_get);
-
 // POST request to delete Enquiry.
-router.post('/enquiry/:id/delete', enquiry_controller.enquiry_delete_post);
-
-// GET request to update Enquiry.
-router.get('/enquiry/:id/update', enquiry_controller.enquiry_update_get);
+router.get('/enquiry/:id/delete', enquiry_controller.enquiry_delete_post);
 
 // POST request to update Enquiry.
 router.post('/enquiry/:id/update', enquiry_controller.enquiry_update_post);
@@ -91,28 +89,23 @@ router.post('/enquiry/:id/update', enquiry_controller.enquiry_update_post);
 router.get('/enquiry/:id', enquiry_controller.enquiry_detail);
 
 // GET request for list of all Enquiry items.
-router.get('/enquiries', enquiry_controller.enquiry_list);
+router.get('/dashboard/enquiries', enquiry_controller.enquiry_list);
 
-// GET request for list of all Enquiry items.
+// GET request for list of all unread Enquiry items.
 router.get('/dashboard', enquiry_controller.dashboard_list);
 
 
 /// PRODUCT ROUTES ///
 
-// GET request for creating product. NOTE This must come before route for id (i.e. display product).
-router.get('/product/create', product_controller.product_create_get);
+
+// GET request for creating product.
+router.get('/product/create', product_controller.product_edit);
 
 // POST request for creating product.
 router.post('/product/create', product_controller.product_create_post);
 
-// GET request to delete product.
-router.get('/product/:id/delete', product_controller.product_delete_get);
-
 // POST request to delete product.
-router.post('/product/:id/delete', product_controller.product_delete_post);
-
-// GET request to update product.
-router.get('/product/:id/update', product_controller.product_update_get);
+router.get('/product/:id/delete', product_controller.product_delete_post);
 
 // POST request to update product.
 router.post('/product/:id/update', product_controller.product_update_post);
@@ -121,11 +114,39 @@ router.post('/product/:id/update', product_controller.product_update_post);
 router.get('/product/:id', product_controller.product_detail);
 
 // GET request for list of all products.
-router.get('/products', product_controller.product_list);
+router.get('/dashboard/products', product_controller.product_edit);
 
-// GET request for list of all products.
-router.get('/products/edit', product_controller.product_edit);
+router.get('/shop', product_controller.product_list);
 
+
+
+/// PROJECT ROUTES ///
+
+// GET request for creating project.
+router.get('/project/create', project_controller.project_edit);
+
+// POST request for creating project.
+router.post('/project/create', project_controller.project_create_post);
+
+// POST request to delete project.
+router.get('/project/:id/delete', project_controller.project_delete_post);
+
+// POST request to update project.
+router.post('/project/:id/update', project_controller.project_update_post);
+
+// GET request for one project.
+router.get('/project/:id', project_controller.project_detail);
+
+// GET request for list of all project.
+router.get('/dashboard/projects', project_controller.project_edit);
+
+router.get('/gallery', function (req, res) {
+    res.render('gallery');
+});
+
+
+
+/* Will be implemented in future if needed ---- *
 /// CATEGORY ROUTES ///
 
 // GET request for creating a category. NOTE This must come before route that displays category (uses id).
@@ -152,31 +173,9 @@ router.get('/productcategory/:id', productcategory_controller.productcategory_de
 // GET request for list of all category.
 router.get('/productcategories', productcategory_controller.productcategory_list);
 
-/// PROJECT ROUTES ///
+/* */
 
-// GET request for creating a project. NOTE This must come before route that displays project (uses id).
-router.get('/project/create', project_controller.project_create_get);
 
-// POST request for creating project.
-router.post('/project/create', project_controller.project_create_post);
-
-// GET request to delete project.
-router.get('/project/:id/delete', project_controller.project_delete_get);
-
-// POST request to delete project.
-router.post('/project/:id/delete', project_controller.project_delete_post);
-
-// GET request to update project.
-router.get('/project/:id/update', project_controller.project_update_get);
-
-// POST request to update project.
-router.post('/project/:id/update', project_controller.project_update_post);
-
-// GET request for one project.
-router.get('/project/:id', project_controller.project_detail);
-
-// GET request for list of all project.
-router.get('/projects', project_controller.project_list);
 
 
 
