f685f3a24132fc514fde91e0c6ade412b644fd54
krushndayshmookh <krushndayshmookh@gmail.com>
2018-06-11 22:31:08 +0530

    masonry

--- www/js/shop.js
+++ www/js/shop.js
@@ -2,6 +2,10 @@
 
 function enquire(id) {
 
+    var username = localStorage.getItem('username');
+    var email = localStorage.getItem('email');
+    var phone = localStorage.getItem('phone');
+
     $.get('/product/' + id, function (product) {
         M.toast({
             html: 'Product Found ' + product.name
@@ -10,3 +14,36 @@ function enquire(id) {
 
 
 }
+
+
+
+/* eslint-env browser, jquery */
+
+(function ($) {
+    $(function () {
+
+        
+
+        // Masonry Grid
+        var $masonry = $('.shop');
+        $masonry.masonry({
+            // set itemSelector so .grid-sizer is not used in layout
+            itemSelector: '.shop-item',
+            // use element for option
+            columnWidth: '.shop-item',
+            // no transitions
+            transitionDuration: 0
+        });
+        // layout Masonry after each image loads
+        $masonry.imagesLoaded(function () {
+            $masonry.masonry('layout');
+        });
+        $('a.filter').click(function (e) {
+            e.preventDefault();
+        });
+
+
+
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
